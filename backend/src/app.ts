dd3d090ce00f2b3a86020136e4e5da2d0cd7a6cd
Yephone <macbookairm1@MacBooks-MacBook-Air.local>
2024-02-27 10:08:31 +0630

    minor changes in app and model with some new lib

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,21 +1,29 @@
 import "dotenv/config";
-import express from "express";
-import NoteModel from "./model/node";
+import express, { NextFunction, Request, Response } from "express";
+import notesRoute from "./routes/notesR";
+import morgan from "morgan";
 
 const app = express();
 
-app.get("/", async (req, res) => {
-  const notes = await NoteModel.find().exec();
-  res.json(notes);
+app.use(morgan("dev"));
+app.use(express.json());
+
+app.use("/api/notes", notesRoute);
+
+app.use((req, res, next) => {
+  if (res.status(404)) {
+    console.log("EndPoint not found ");
+    next({ message: "This route doesnt exist " });
+  }
 });
 
-app.post("/addNote", async (req, res) => {
-  const note = new NoteModel({
-    title: "About me tho",
-    text: "Your life real  ?",
-  });
-  await note.save();
-  res.json(note);
+app.use((error: unknown, req: Request, res: Response, next: NextFunction) => {
+  console.log(error);
+  let errorMessage = "Error fetching notes";
+  if (error instanceof Error) {
+    errorMessage = error.message;
+  }
+  res.status(500).json({ message: errorMessage });
 });
 
 export default app;
